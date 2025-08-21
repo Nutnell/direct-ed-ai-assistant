5fe77bc2f2d3585c503bd39de5aafc17ddc6ac93
Nutnell <nutnell00@gmail.com>
2025-08-21 10:03:32 +0300

    Added CORS middleware

--- app/server.py
+++ app/server.py
@@ -2,8 +2,9 @@
 
 from fastapi import FastAPI
 from langserve import add_routes
+from fastapi.middleware.cors import CORSMiddleware
 
-# Importing memory-enabled router chain
+# memory-enabled router chain
 from app.chains.router import router_chain_with_history
 
 # FastAPI App Initialization
@@ -13,9 +14,16 @@ app = FastAPI(
     description="A multi-functional API server for the DirectEd AI assistant.",
 )
 
-# LangServe Routes
+# Add CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
 
-# final chain at the /chat endpoint
+# LangServe Routes
 add_routes(
     app,
     router_chain_with_history,
